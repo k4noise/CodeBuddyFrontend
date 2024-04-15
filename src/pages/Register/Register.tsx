827bd1d0465d72964926ab50d036845a8e8e730f
k4noise <vestertp@gmail.com>
2024-04-15 22:59:16 +0500

    add: register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,55 @@
+import LeaningManImage from '../../assets/leaning-man.png';
+import './Register.css';
+
 const Register = () => {
-  return <h2>Register form</h2>;
+  return (
+    <div className="register">
+      <div className="register__wrapper">
+        <img src={LeaningManImage} alt="leaning man" />
+        <form action="post" className="register__form">
+          <h2 className="register__form-header">Регистрация</h2>
+          <fieldset className="register__form-fieldset">
+            <label>
+              <input type="radio" name="type" />Я студент
+            </label>
+            <label>
+              <input type="radio" name="type" />Я ментор
+            </label>
+          </fieldset>
+          <input
+            type="text"
+            placeholder="Имя"
+            className="register__form-field"
+          />
+          <input
+            type="text"
+            placeholder="Фамилия"
+            className="register__form-field"
+          />
+          <input
+            type="email"
+            placeholder="Почта"
+            className="register__form-field"
+          />
+          <input
+            type="password"
+            placeholder="Пароль"
+            className="register__form-field"
+          />
+          <input
+            type="password"
+            placeholder="Пароль еще раз"
+            className="register__form-field"
+          />
+          <label className="register__form-policy">
+            <input type="checkbox" />Я согласен с условиями политики
+            конфиденциальности
+          </label>
+          <button className="register__form-send">Зарегестрироваться</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
