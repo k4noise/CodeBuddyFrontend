1888fd6b61c326ab4a803d95af927faacb55af60
k4noise <vestertp@gmail.com>
2024-04-16 23:28:40 +0500

    add: login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,41 @@
+import './Login.css';
+import ManWithLaptopImage from '../../assets/man-with-laptop.png';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-  return <h2>Login form</h2>;
+  return (
+    <div className="login">
+      <div className="login__wrapper">
+        <img src={ManWithLaptopImage} alt="man with laptop" />
+        <form className="login__form">
+          <fieldset className="login__form-fieldset form__fieldset">
+            <label>
+              <input type="radio" name="type" />Я студент
+            </label>
+            <label>
+              <input type="radio" name="type" />Я ментор
+            </label>
+          </fieldset>
+          <input
+            type="email"
+            placeholder="Почта"
+            className="login__form-field form__field"
+          />
+          <input
+            type="password"
+            placeholder="Пароль"
+            className="login__form-field form__field"
+          />
+          <div className="login__form-actions">
+            <button className="login__form-button">Войти</button>
+            <Link to="/register" className="login__form-link">
+              Нет аккаунта?
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
