efdd3cc9a38d7ccc2a91c32d7781156553d92977
k4noise <vestertp@gmail.com>
2024-04-27 23:49:00 +0500

    migrate: swc -> babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
+    setupFiles: ['./test-setup.ts'],
     environment: 'jsdom',
   },
 });
